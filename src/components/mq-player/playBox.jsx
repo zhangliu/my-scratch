507ec0159a13cc95cf33586c224633c498d317ba
yunnian.zhang <yunnian.zhang@yintech.cn>
2018-05-19 21:07:18 +0800

    modify

--- src/components/mq-player/playBox.jsx
+++ src/components/mq-player/playBox.jsx
@@ -2,33 +2,19 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ReactModal from 'react-modal';
 import Box from '../box/box.jsx';
-import videojs from 'video.js';
+// import videojs from 'video.js';
 
+// see https://segmentfault.com/a/1190000007603266
+// import SWF_PATH from 'video.js/dist/video-js.swf';
+// videojs.options.flash.swf = SWF_PATH;
+
+// import 'video.js/dist/video-js.css';
 import styles from './index.css';
 
 export default class MyComponent extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { isOpen: true} // props.showPlayBox }
-    }
-
-    componentDidMount() {
-        if (this.player) return this.player.play()
-        const videoJsOptions = {
-            autoplay: true,
-            controls: true,
-            sources: [{
-                src: 'http://www.codingmarch.com/statics/video/trylesson.mp4',
-                type: 'video/mp4'
-            }]
-        }
-        this.player = videojs(this.videoNode, videoJsOptions, () => {
-            const children = this.player.children()
-            const video = children.find(c => !!c.currentSrc);
-            video.className = video.className
-                ? `${video.className} ${styles.mqVideo}`
-                : styles.mqVideo
-        });
+        this.state = { isOpen: true } // props.showPlayBox }
     }
 
     componentWillReceiveProps(props) {
@@ -36,11 +22,20 @@ export default class MyComponent extends React.Component {
         this.setState({isOpen: props.isOpen})
     }
 
+    componentDidMount() {
+        this.player.play();
+    }
+
     // destroy player on unmount
     componentWillUnmount() {
         if (this.player) {
             this.player.dispose();
         }
+    }
+
+    handVideoClick() {
+        if (this.player.paused) return this.player.play()
+        this.player.pause()
     }
 
     show() {
@@ -59,40 +54,17 @@ export default class MyComponent extends React.Component {
             <div className={isOpen ? styles.mqModalOverlay : styles.mqModalOverlayHidden}>
                 <div className={styles.mqModalContent}>
                     <div className={styles.modalClose} onClick={() => this.hide()}>X</div>
-                    <div data-vjs-player>
-                        <video
-                            className={styles.mqVideoJs}
-                            ref={node => this.videoNode = this.videoNode || node}
-                        />
-                    </div>
+                    <video
+                        src='http://www.codingmarch.com/statics/video/trylesson.mp4'
+                        controls
+                        onClick={this.handVideoClick.bind(this)}
+                        ref={node => this.player = node}
+                        className={styles.mqVideo}
+                    >
+                        your browser does not support the video tag
+                    </video>
                 </div>
             </div>
         )
     }
-    // render() {
-    //     return (<ReactModal
-    //         isOpen={this.state.isOpen}
-    //         className={styles.mqModalContent}
-    //         contentLabel=''
-    //         overlayClassName={styles.mqModalOverlay}
-    //         onRequestClose={() => this.hide()}
-    //         onAfterOpen={this.onOpenModal.bind(this)}
-    //     >
-    //         <Box className={styles.mqPlayBoxBody}>
-    //             <div data-vjs-player>
-    //                 <video
-    //                     className={styles.mqVideoJs}
-    //                     ref={node => this.videoNode = this.videoNode || node}
-    //                 />
-    //             </div>
-    //         </Box>
-    //     </ReactModal>)
-    // }
 }
-
-// MyComponent.propTypes = {
-//     onCancel: PropTypes.func.isRequired,
-//     onTryIt: PropTypes.func.isRequired
-// };
-
-// export default MyComponent;
